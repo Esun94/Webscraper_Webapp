f90a4c77b043930ca83270bf85c28006f4e27671
Eric Sun <ericmsun@gmail.com>
2022-08-03 12:53:53 -0400

    GET method to populate JSON response/results to Port800- front end

--- index.js
+++ index.js
@@ -7,22 +7,35 @@ const app = express()
 
 const url = 'https://www.theguardian.com/us';
 
-axios(url)
-    .then(response => {
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const articles = []; 
+//METHOD = get,post,put,delete
+//PATH = example /burger or endpoint
+//HANDLER function to be executed
 
-        $('.fc-item__title', html).each(function() {
-            const title = $(this).text();
-            const url = $(this).find('a').attr('href');
-            articles.push({
-                title,
-                url
-            })
-        })
-        console.log(articles)
-    })
+// app.METHOD(PATH, HANDLER);
 
+// app.get('/', function (req, res) {
+//     res.json(`ERIC's Webscraper Webapp !!`)
+// })
+
+app.get('/results', function (req, res) {
+    axios(url)
+        .then(response => {
+            const html = response.data;
+            const $ = cheerio.load(html);
+            const articles = []; 
+    
+            $('.fc-item__title', html).each(function() {
+                let title = $(this).text();
+                let url = $(this).find('a').attr('href');
+                articles.push({
+                    title,
+                    url
+                });
+            });
+            // console.log(articles)
+            res.json(articles)
+        }).catch(err => console.log(err));
+    
+});
 
 app.listen(PORT, () => console.log(`server listening to PORT ${PORT}`))
