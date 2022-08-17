662c70217f4e1f004fc01e22ca1f54aeeaaa3310
Eric Sun <ericmsun@gmail.com>
2022-08-17 12:31:52 -0400

    json2csv dependency, csv able to generate but HTML constantly reloads, commented out json2csv code

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const axios = require('axios');
 const express = require('express');
 const cheerio = require('cheerio');
 const cors = require('cors');
+// const json2csv = require('json2csv').Parser;
+// const fs = require('fs');
+
 const app = express()
 
 app.use(cors());
 
-const url = 'https://en.wikipedia.org/wiki/Anonymous_(hacker_group)';
+const url = 'https://en.wikipedia.org/wiki/James_Bond';
 
 
 app.get('/results', function (req, res) {
@@ -17,7 +20,6 @@ app.get('/results', function (req, res) {
             const $ = cheerio.load(html);
             const wikiResult = [];
 
-            
             $( 'h2, p', html).each(function() {
                 let subTitle = $(this).find('.mw-headline').text();
                 let paragraph = $(this).text();
@@ -26,8 +28,21 @@ app.get('/results', function (req, res) {
                     paragraph,
                 });
             }),
-            res.json(wikiResult)
+            res.json(wikiResult);
+
+            // const j2cp = new json2csv();
+            // const csv = j2cp.parse(wikiResult);
+
+            // fs.writeFileSync("./output.csv", csv, "utf-8");
+
         }).catch(err => console.log(err));
-});
+        
+    });
+
+
+
+
+
+
 
 app.listen(PORT, () => console.log(`server listening to PORT ${PORT}`))
